4e9ba0009f7c74e3ce1233e65f15431ccfdd22d8
Omer Katz <omerk2511@gmail.com>
2019-08-15 08:24:30 -0700

    Created a procedure that switches from 16-bit real mode to 32-bit protected mode. It clears the interrupt flag, loads the GDT, sets the lsb of cr0, and does a far jump to another label where the segment registers are initialized and the stack is updated.

---
+++ bootloader/protected_mode/switch_to_pm.asm
@@ -0,0 +1,30 @@
+[bits 16]
+
+; Switches the system from 16-bit real mode
+; to 32-bit protected mode
+switch_to_pm:
+	cli
+
+	lgdt [gdt_descriptor]
+
+	mov eax, cr0
+	or cr0, 0x01
+	mov cr0, eax
+
+	jmp CODE_SEG:init_pm
+
+
+[bits 32]
+
+init_pm:
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov gs, ax
+	mov fs, ax
+	
+	mov ebp, 0x90000
+	mov esp, ebp
+
+	call begin_pm
