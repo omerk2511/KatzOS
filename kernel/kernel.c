a210f521c57fd1d351ab566a5c7d8d8be75eaf94
Omer Katz <omerk2511@gmail.com>
2019-08-16 15:19:46 -0700

    Created a kernel entry in order to make sure that the kernel starts at main, and refactored the kernel to clear the screen and display a success message.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,17 @@
 void main()
 {
 	char *video_memory = (char *) 0xb8000;
-	*video_memory = 'X';
+	
+	// Screen clearing
+	for(int i = 0; i < 80 * 25 * 2; i += 2) {
+		video_memory[i] = ' ';
+		video_memory[i + 1] = 0x07;
+	}
+
+	// Basic message displaying
+	char *kernel_message = "Started the kernel successfully!";
+	
+	for(int i = 0; kernel_message[i] != '\0'; i++) {
+		video_memory[i * 2] = kernel_message[i];
+	}
 }
