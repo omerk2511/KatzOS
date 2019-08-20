4a409899d311cf0891761a5fdd763c46fb88d85a
Omer Katz <omerk2511@gmail.com>
2019-08-20 03:22:54 -0700

    Fixed the port_byte_out function.

--- kernel/io.c
+++ kernel/io.c
@@ -11,5 +11,5 @@ unsigned char port_byte_in(unsigned short port)
 // Outputs byte-long data to a specific I/O port
 void port_byte_out(unsigned short port, unsigned char data)
 {
-	__asm__("out %%al, %%dx" : "=a" (data) : "d" (port));
+	__asm__("out %%al, %%dx" : :"a" (data), "d" (port));
 }
